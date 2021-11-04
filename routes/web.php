9134580823c2b6d35b39bfbf4b7fa91b17d998fc
anatkh <384535415@qq.com>
2021-11-04 14:04:52 +0800

    '基础页面'

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\StaticPageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPageController@home');
+Route::get('/help','StaticPageController@help');
+Route::get('/about','StaticPageController@about');
